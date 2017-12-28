07bbb6b1b0ba272e87fba1952f204910c21fabfe
Binod Raj Pandey <binod@fusemachines.com>
2017-12-28 07:40:00 +0545

    some changes in app file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username = 'Binod';
+  username: string;
+  email: string;
+  address: Address;
+  hobbies: string[];
+  showHobbies: boolean;
+  constructor(){
+    this.username='Binod';
+    this.email='binodrajpandey@ioe.edu.np';
+    this.address={
+      country: 'Nepal',
+      district: 'Nuwakot'
+    },
+    this.hobbies=['chess','football','cricket'];
+    this.showHobbies=false;
+  }
+  showHobby(){
+    this.showHobbies=true;
+  }
+}
+interface Address{
+country: string;
+district: string;
+
 }
