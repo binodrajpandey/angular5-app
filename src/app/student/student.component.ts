bafad60d5dd573a8d9cfe5aaa4cce1a14f0df331
Binod Raj Pandey <binod@fusemachines.com>
2018-01-09 20:09:11 +0545

    login service

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-student',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService:AppService) { 
+    this.appService.checkCredential();
+  }
 
   ngOnInit() {
   }
